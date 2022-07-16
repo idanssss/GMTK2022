679c438088b624fa0753528f5a946373e1672a92
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-07-16 23:22:51 +0300

    Done with enemies
    fixed main game scene box colliders and everything needed to make enemies work

--- WaveManager.cs
+++ WaveManager.cs
@@ -7,14 +7,21 @@ public class WaveManager : MonoBehaviour
     [SerializeField]
     AnimationCurve enemyByPhase;
     public GameObject EnemyPrefab;
+    public TileManager tm;
 
     int phase;
+
+    void Start()
+    {
+        tm.OnNewBoard += SummonNextWave;
+        SummonNextWave();
+    }
 
     public void SummonNextWave()
     {
         for(int i = 0; i < Mathf.RoundToInt(enemyByPhase.Evaluate(phase+1)); i++)
         {
-            Instantiate(EnemyPrefab, new Vector2(Random.Range(-7, 7), Random.Range(-2, 3)), Quaternion.identity);
+            Instantiate(EnemyPrefab, new Vector2(Random.Range(-4, 4), Random.Range(-2, 3)), Quaternion.identity);
         }
         phase++;
     }
