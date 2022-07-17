13c11068d7fd34a8a6dbf7d9a7adf62eab9ca329
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-07-17 14:00:12 +0300

    added fonts and main menu

---
+++ PersonalizedButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PersonalizedButton : MonoBehaviour
+{
+    public bool Animated;
+    public UnityEvent OnClick;
+    
+    void OnMouseOver()
+    {
+
+    }
+    void OnMouseDown()
+    {
+
+    }
+}
