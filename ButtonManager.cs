9764c0e00b7a22cda87698da21e03bebb1c674ab
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-07-17 18:51:37 +0300

    fixed some minor bugs and added pause menu

--- ButtonManager.cs
+++ ButtonManager.cs
@@ -8,4 +8,10 @@ public class ButtonManager : MonoBehaviour
     public void StartGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     public void QuitGame() => Application.Quit();
+
+    public void Resume()
+    {
+        GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
 }
--- DeathTrigger.cs
+++ DeathTrigger.cs
@@ -6,6 +6,7 @@ public class DeathTrigger : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.CompareTag("Bullet")) return;
         Destroy(other.gameObject);
     }
 }
