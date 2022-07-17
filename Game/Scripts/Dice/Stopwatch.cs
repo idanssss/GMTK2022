285d7479bb746bbe9c383509b9188e3b977d63de
TrimericDragon7 <36777029+idanssss@users.noreply.github.com>
2022-07-17 12:18:09 +0300

    Added Stopwatch

--- Game/Scripts/Dice/Stopwatch.cs
+++ Game/Scripts/Dice/Stopwatch.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class Stopwatch : MonoBehaviour
     [SerializeField] private Dice dice;
     [SerializeField] private float time;
     [SerializeField] private float timeToStart;
+    [SerializeField] private SpriteRenderer rend;
+    [SerializeField] private List<Sprite> states = new List<Sprite>();
 
     public bool count = true;
 
@@ -20,6 +23,12 @@ public class Stopwatch : MonoBehaviour
         
         time -= Time.deltaTime;
         timerText.text = time.ToString("0.00");
+
+        float eighth = timeToStart / 8;
+        int state = (int)(time / eighth);
+        
+        rend.sprite = states[state];
+        Debug.Log(rend.sprite);
 
         if (time <= 0)
         {
