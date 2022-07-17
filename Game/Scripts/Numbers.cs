59700abe12263dc3ab36250bd6deb5dabd0293db
TrimericDragon7 <36777029+idanssss@users.noreply.github.com>
2022-07-17 15:44:56 +0300

    Revert "made number loading work using Resources.Load"
    This reverts commit 160e0e36cb2aea4eafc1549eeb5e299ed7648c41.

--- Game/Scripts/Numbers.cs
+++ Game/Scripts/Numbers.cs
@@ -4,15 +4,14 @@ using UnityEngine.Assertions;
 
 public static class Numbers
 {
-    private const string root = "Numbers_";
+    private const string root = "Assets/GMTK2022/Game/Sprites/Numbers/Numbers/Numbers Prefab/Numbers_";
 
     public static Sprite GetSprite(int digit)
     {
         Assert.IsTrue(digit >= 0 && digit <= 9, "Digit is outside of bounds!");
         
         string name = root + digit + ".prefab";
-        //var p = AssetDatabase.LoadAssetAtPath<GameObject>(name);
-        var p = (GameObject) Resources.Load(root + digit + ".prefab");
+        var p = AssetDatabase.LoadAssetAtPath<GameObject>(name);
         var sprite = p.GetComponent<SpriteRenderer>().sprite;
 
         return sprite;
