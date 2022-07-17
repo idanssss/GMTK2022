160e0e36cb2aea4eafc1549eeb5e299ed7648c41
DarknessRisesFromBelow <spam.me.please.do.it@gmail.com>
2022-07-17 15:23:24 +0300

    made number loading work using Resources.Load

--- Game/Scripts/Numbers.cs
+++ Game/Scripts/Numbers.cs
@@ -4,14 +4,15 @@ using UnityEngine.Assertions;
 
 public static class Numbers
 {
-    private const string root = "Assets/GMTK2022/Game/Sprites/Numbers/Numbers/Numbers Prefab/Numbers_";
+    private const string root = "Numbers_";
 
     public static Sprite GetSprite(int digit)
     {
         Assert.IsTrue(digit >= 0 && digit <= 9, "Digit is outside of bounds!");
         
         string name = root + digit + ".prefab";
-        var p = AssetDatabase.LoadAssetAtPath<GameObject>(name);
+        //var p = AssetDatabase.LoadAssetAtPath<GameObject>(name);
+        var p = (GameObject) Resources.Load(root + digit + ".prefab");
         var sprite = p.GetComponent<SpriteRenderer>().sprite;
 
         return sprite;
