fe0d7db1c22c74c5904c1d56b89c645cc5a32b2a
TrimericDragon7 <36777029+idanssss@users.noreply.github.com>
2022-07-17 21:50:24 +0300

    aijsdnjsan

--- Game/Scripts/Character/CharacterHealth.cs
+++ Game/Scripts/Character/CharacterHealth.cs
@@ -55,6 +55,69 @@ using UnityEngine.UI;
 using Action = System.Action;
 using Assert = UnityEngine.Assertions.Assert;
 
+// public class CharacterHealth : MonoBehaviour
+// {
+//     
+//     // public GameObject gameOverMenu;
+//     // public Slider HPslider;
+//     //
+//     // private float _health;
+//     // public float Health
+//     // {
+//     //     get => _health;
+//     //     private set
+//     //     {
+//     //         value = value < 0 ? 0 : value;
+//     //         if (Mathf.Abs(_health - value) < 0.0001f) return;
+//     //
+//     //         _health = value;
+//     //
+//     //
+//     //
+//     //         if (value <= 0)
+//     //         {
+//     //             if (transform.name == "Player")
+//     //             {
+//     //                 OnDeath?.Invoke();
+//     //                 Destroy(gameObject);
+//     //             }
+//     //         }
+//     //     }
+//     // }
+//     //
+//     // [SerializeField] private float startHealth = 100f;
+//     //
+//     // private void Awake()
+//     // {
+//     //     Assert.IsTrue(startHealth > 0, "Start health must be greater than 0");
+//     //     _health = startHealth;
+//     //     OnDeath += GameOver;
+//     // }
+//     //
+//     // public event Action OnDeath;
+//     //
+//     // public delegate void GetHitDel(GameObject go);
+//     // public event GetHitDel OnGetHit;
+//     //
+//     // public void Hit(float damage, GameObject go)
+//     // {
+//     //     Health -= damage;
+//     //     OnGetHit?.Invoke(go);
+//     //     if(transform.name == "Player")
+//     //         // HPslider.value = 100 / (100 / (startHealth / _health));
+//     //         HPslider.value = Health;
+//     // }
+//     //
+//     // private void GameOver()
+//     // {
+//     //     gameOverMenu.SetActive(true);
+//     // }
+// }
+
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
 public class CharacterHealth : MonoBehaviour
 {
     public GameObject gameOverMenu;
@@ -80,6 +143,9 @@ public class CharacterHealth : MonoBehaviour
                     OnDeath?.Invoke();
                     Debug.Log("asidsajidjkdasn");
                 }
+                
+                
+                Destroy(gameObject);
             }
         }
     }
@@ -88,7 +154,7 @@ public class CharacterHealth : MonoBehaviour
 
     private void Awake()
     {
-        Assert.IsTrue(startHealth > 0, "Start health must be greater than 0");
+        UnityEngine.Assertions.Assert.IsTrue(startHealth > 0, "Start health must be greater than 0");
         _health = startHealth;
         OnDeath += GameOver;
     }
